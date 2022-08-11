29122fbab3590ac566a7ab1491fdc9f77f9c5fde
ALASHI1 <alashimuyiwa@gmail.com>
2022-08-11 14:36:03 +0100

    sorts

---
+++ 100-shell_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * Knuth sequence: n+1 = n * 3 + 1
+ * 1, 4, 13, 40, 121, ...
+ * @array: given array
+ * @size: size
+ */
+void shell_sort(int *array, size_t size)
+{
+    size_t i, j, h;
+    int temp;
+
+    if (size < 2)
+        return;
+
+    /* get initial maximum gap (h) */
+    h = 1;
+    while (h < size)
+        h = h * 3 + 1;
+
+    while ((h /= 3) >= 1)
+    {
+        for (i = h; i < size; i++)
+        {
+            for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+            {
+                temp = array[j];
+                array[j] = array[j - h];
+                array[j - h] = temp;
+            }
+        }
+        print_array(array, size);
+    }
+}
---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+
+/**
+ * swap - swap two nodes in a doubly linked list
+ * @list: given list
+ * @A: first node
+ * @B: second node
+ */
+void swap(listint_t **list, listint_t *A, listint_t *B)
+{
+    if (A == B)
+        return;
+
+    A->next = B->next;
+    B->prev = A->prev;
+
+    if (A->prev)
+        A->prev->next = B;
+    else
+        *list = B;
+
+    if (B->next)
+        B->next->prev = A;
+
+    A->prev = B;
+    B->next = A;
+    print_list(*list);
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers in
+ * ascending order using the Cocktail shaker sort algorithm
+ * @list: given list
+ *
+ * [*] Performance update
+ * since in the forward bubble, the last index will be the max
+ * of the list, to improve performance, we set a upper boundary
+ * so the algorithm never reach there again. Same for the backward
+ * bubble.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+    listint_t *current, *next, *prev;
+    listint_t *lower, *upper;
+    int swapped = 0;
+
+    if (!list || !(*list) || !((*list)->next))
+        return;
+
+    do
+    {
+        current = *list;
+        /* do forward bubble */
+        while (current && current->next && current->next != upper)
+        {
+            next = current->next;
+            if (current->n > next->n)
+            {
+                swap(list, current, next);
+                swapped = 1;
+                continue;
+            }
+            current = current->next;
+        }
+        upper = current;
+        /* will a swap still be necessary? */
+        if (!swapped)
+            break;
+        swapped = 0;
+        /* do backward bubble */
+        while (current && current->prev && current->prev != lower)
+        {
+            prev = current->prev;
+            if (prev->n > current->n)
+            {
+                swap(list, prev, current);
+                swapped = 1;
+                continue;
+            }
+            current = current->prev;
+        }
+        lower = current;
+    } while (swapped);
+}
